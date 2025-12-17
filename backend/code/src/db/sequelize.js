30c5ab17e20eff7298d5f36db12453ab60143178
ASETML <alban.segalen@eduvaud.ch>
2025-12-17 15:10:01 +0100

    Lire le certificat en binaire

--- backend/code/src/db/sequelize.js
+++ backend/code/src/db/sequelize.js
@@ -26,7 +26,7 @@ const sequelize = new Sequelize(
       required: true,
       rejectUnauthorized: false,
       minVersion: "TLSv1.2",
-      ca: process.env.CA
+      ca: process.env.CA ? Buffer.from(process.env.CA, 'utf-8') : null
     }
   }
 );
